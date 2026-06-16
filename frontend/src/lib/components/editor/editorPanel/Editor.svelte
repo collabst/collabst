1214b9e5bc871cf940748db33c81f8101c1bd268
gdamms <damguillotin@gmail.com>
2026-06-16 17:50:21 +0200

    wip

--- frontend/src/lib/components/editor/editorPanel/Editor.svelte
+++ frontend/src/lib/components/editor/editorPanel/Editor.svelte
@@ -9,9 +9,33 @@
     flex: 1;
     height: 100%;
     overflow: hidden;
+    background-color: var(--bg-editor);
   }
 
-  :global(.cm-scroller) {
+  .editor :global(.cm-gutter) {
+    background-color: var(--bg-editor);
+  }
+
+  .editor :global(.cm-gutters) {
+    border: none;
+  }
+
+  .editor :global(.cm-scroller) {
     padding-top: 3rem;
+  }
+
+  :global([data-theme="dark"]) .editor :global(.cm-activeLine),
+  :global([data-theme="dark"]) .editor :global(.cm-activeLineGutter) {
+    background-color: #b0bac71a;
+  }
+
+  :global([data-theme="light"]) .editor :global(.cm-activeLine),
+  :global([data-theme="light"]) .editor :global(.cm-activeLineGutter) {
+    background-color: #06162f0b;
+  }
+
+  .editor :global(.cm-cursor) {
+    border-left-color: var(--color-primary-500);
+    border-left-width: 2px;
   }
 </style>
